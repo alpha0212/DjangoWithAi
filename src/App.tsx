de4acd0ac61e6e205ddbc524ab3742b4f896f277
alpha0212 <qkrcjfgml321@naver.com>
2023-03-07 09:17:58 +0900

    🔨 testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Webcam from 'react-webcam';
+import styled from 'styled-components';
 //375, 675
 const videoConstraints = {
   width: 375,
@@ -7,24 +8,35 @@ const videoConstraints = {
   facingMode: 'environment',
 };
 const App = () => {
+  const [imgSrc, setImgSrc] = useState('');
   const cameraRef = React.useRef(null) as any;
   const capture = React.useCallback(() => {
     const imageSrc = cameraRef.current.getScreenshot();
+    setImgSrc(imageSrc);
     console.log(imageSrc);
   }, [cameraRef]);
 
   return (
     <div className="App">
-      <Webcam
-        audio={false}
-        height={videoConstraints.height}
-        ref={cameraRef}
-        screenshotFormat="image/jpeg"
-        width={videoConstraints.width}
-        videoConstraints={videoConstraints}
-      />
+      {imgSrc ? (
+        <CaptureResult src={imgSrc} />
+      ) : (
+        <Webcam
+          audio={false}
+          height={videoConstraints.height}
+          ref={cameraRef}
+          screenshotFormat="image/jpeg"
+          width={videoConstraints.width}
+          videoConstraints={videoConstraints}
+        />
+      )}
       <button onClick={capture}>Capture photo</button>
     </div>
   );
 };
+
+export const CaptureResult = styled.img`
+  width: 100px
+  height: 100px;
+`;
 export default App;
